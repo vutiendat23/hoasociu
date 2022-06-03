50f18e29406b82048c651e02e10f552e7fefb79c
vutiendat23 <106805828+vutiendat23@users.noreply.github.com>
2022-06-03 16:20:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hi Nguyễn Quỳnh Hoa 🖐🖐🖐',
+    introDesc: `đạt có điều muốn nói, hoa phải trả lời thật lòng đó`,
+    btnIntro: 'vâng',
+    title: 'hoa thích đạt phải không phải không ? 😙',
+    desc: 'hoa không trả lời hoặc thoát ra, tức là hoa thích đạt đó nha :> ',
+    btnYes: 'Có, hoa thật sự thích đạt 💖💖💖',
+    btnNo: '💔 đạt mơ à ! 💔 ',
+    question:'Nói cho đạt biết đi vì sao hoa thích đạt <3',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì đạt rất rất đẹp traiiiiiiiiiiii 😊😊',
+    mess: 'đạt biết mà, đạt cũng thích hoa lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho đạt nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/profile.php?id=100013126022097'
+}
